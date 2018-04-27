936b1f0aa22dfb032aac4cfe9c133a45bca18c24
macko992 <m4ck0992@gmail.com>
2018-04-27 17:31:34 +0200

    CSS file: Change buttons from class to id. Removed: .center .overlay. Addded: .container .aminateLeft. In modal added function to writing text under main button.

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,18 @@
-var overlay = document.getElementById('overlay');
+var overlay = document.getElementById('overlay'); //get all from div with id overlay
 
-function openModal(){
-	overlay.classList.remove("is-hidden");
+//function to open modal
+function openModal() {
+  overlay.classList.remove("is-hidden");
 }
-
-function closeModal(){
-	overlay.classList.add("is-hidden");
+//function to close modal after click 'cancel'
+function closeModal() {
+	var textNone = document.getElementById('showText');
+  overlay.classList.add("is-hidden");
+	textNone.innerHTML="";
 }
-
-function applyModal(){
-	var show = 'Gra gitara';
-	overlay.classList.add("is-hiden");
-	
+//function to close modal and show text after click 'ok'
+function applyModal() {
+  var text = document.getElementById('showText');
+  overlay.classList.add("is-hidden");
+  text.innerHTML = "piknie Panie to żeś uczynił </br> (chyba  :| )";
 }
