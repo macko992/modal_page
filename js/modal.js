09e456b850d3bb5b34dfde24ee33d7377a2d0dd6
macko992 <m4ck0992@gmail.com>
2018-04-27 09:51:05 +0200

    A modal.js file was Created along with methods for opening and closing the modal.CreatedModify style.css and index.html. Added modla stylles to css file. To index.html added divs connected with buttons and import scripts from modal.js.

---
+++ js/modal.js
@@ -0,0 +1,15 @@
+var overlay = document.getElementById('overlay');
+
+function openModal(){
+	overlay.classList.remove("is-hidden");
+}
+
+function closeModal(){
+	overlay.classList.add("is-hidden");
+}
+
+function applyModal(){
+	var show = 'Gra gitara';
+	overlay.classList.add("is-hiden");
+	
+}
